c7454c8d0c2854d310c5522ffb7fce903ea63789
Scynes <dustin.greyfield@gmail.com>
2023-05-23 18:19:06 -0400

    wip/landing: added object array containing explore topics and general layout skeleton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,32 @@
+<script>
+    const TOPICS = [
+        {
+            title: 'WEB DESIGN',
+            description: 'Learn the basics of web design, including HTML, CSS, and cool design tricks.Get creative with hands-on activities and build your own awesome websites.'
+        },
+        {
+            title: 'ENGINEERING',
+            description: 'Discover the fascinating world of engineering as you dive into hands-on projects and experiments. Learn about design, problem-solving, and teamwork while building amazing structures and inventions.'
+        },
+        {
+            title: 'CODING',
+            description: 'Dive into the captivating world of programming as you learn the basics of coding languages like Python and JavaScript. Unleash your problem-solving skills, create awesome games and interactive projects.'
+        },
+        {
+            title: 'HTML',
+            description: 'Dive into the world of coding as you learn HTML, the language that powers the internet. Discover how to create and structure web pages, add text, images, and links, and bring your own creative ideas to life.'
+        },
+        {
+            title: 'CSS',
+            description: "In this exciting course, you'll learn CSS, the magic behind making web pages look amazing. Discover how to add colors, fonts, and eye-catching layouts to your web creations."
+        },
+        {
+            title: 'JavaScript',
+            description: 'Dive into the world of coding and learn JavaScript, the language that adds life to web pages. Discover how to create dynamic and interactive elements, build fun games, and make your web projects come alive.'
+        }
+    ]
+</script>
+
 <div class="landing-container">
     <section class="landing-hero flex column gap-2rem">
         <h2>Tech E-Learning: Empowering Young Innovators for the Future!</h2>
@@ -5,9 +34,19 @@
         <button class="borderless">Getting Started</button>
     </section>
     <section class="landing-explore">
+        <h2>EXPLORE OUR EXCITING TECH COURSES</h2>
+        <div class="topics">
+            <div class="topic">
+                <h1>WEB DESIGN</h1>
+                <p></p>
+            </div>
+            <div class="topic">
+                <h1>ENGINEERING</h1>
+                <p></p>
+            </div>
 
+        </div>
     </section>
     <section class="landing-about">
-
     </section>
 </div>
