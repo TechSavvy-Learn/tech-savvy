79cc62e01375b1bd61e1fc8f13d41b12da7e0788
Scynes <dustin.greyfield@gmail.com>
2023-05-23 17:19:08 -0400

    wip/landing: added hero text layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <div class="landing-container">
     <section class="landing-hero">
-
+        <h2>Tech E-Learning: Empowering Young Innovators for the Future!</h2>
+        <p>Discover the Thrilling World of Technology with Engaging E-Learning Courses Designed Just for Kids!</p>
+        <button>Getting Started</button>
     </section>
     <section class="landing-explore">
 
     </section>
     <section class="landing-about">
-        
+
     </section>
 </div>
