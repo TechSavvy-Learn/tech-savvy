b6b11e0c40270955ad3edf86785051c30ff991ae
Scynes <dustin.greyfield@gmail.com>
2023-05-23 15:33:14 -0400

    wip: added project CSS and layout component

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			'@public': path.resolve(__dirname, './public'),
+			'@components': path.resolve(__dirname, './src/components')
+		}
+	},
+	server: {
+		fs: {
+			allow: ['..']
+		}
+	}
 });
