8d5c49626ce7059ce7ebb98852bfd3bc8bdc44b4
Scynes <dustin.greyfield@gmail.com>
2023-05-23 17:55:00 -0400

    wip/landing: completed landing hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <div class="landing-container">
-    <section class="landing-hero">
+    <section class="landing-hero flex column gap-2rem">
         <h2>Tech E-Learning: Empowering Young Innovators for the Future!</h2>
         <p>Discover the Thrilling World of Technology with Engaging E-Learning Courses Designed Just for Kids!</p>
-        <button>Getting Started</button>
+        <button class="borderless">Getting Started</button>
     </section>
     <section class="landing-explore">
 
