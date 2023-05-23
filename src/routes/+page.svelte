6f99832ce0f3fc5bcb85f5be9b5ea07d676d0249
Scynes <dustin.greyfield@gmail.com>
2023-05-23 16:54:35 -0400

    wip/landing: added samantic container layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="landing-container">
+    <section class="landing-hero">
+
+    </section>
+    <section class="landing-explore">
+
+    </section>
+    <section class="landing-about">
+        
+    </section>
+</div>
