e89a4dbad9c2f8f9d575a96a64f5637c3bbfa9bc
krsnamara <martinj.fitzpatrick@gmail.com>
2023-05-27 10:37:35 -0400

    updating signup with new nav and responsive. WIP continue small screen icon

--- src/components/common/BackConBtn.svelte
+++ src/components/common/BackConBtn.svelte
@@ -1,28 +1,61 @@
+<svelte:head>
+	<link rel="stylesheet" href="https://unpkg.com/mono-icons@1.0.5/iconfont/icons.css" >
+</svelte:head>
 <style>
-button {
-		background-color: var(--gray-blue);
-        color: white;
-        border-radius: 8px;
-        font-size: 1.8rem;
-        font-weight:500;
-        cursor: pointer;
-        width: 140px;
-        height: 42px;
-        border: none;
-	}
+a{
+    text-decoration: none;
+}
+button, input[type= submit] {
+    background-color: var(--gray-blue);
+    color: white;
+    border-radius: 8px;
+    font-size: 1.8rem;
+    font-weight:500;
+    cursor: pointer;
+    width: 140px;
+    height: 42px;
+    border: none;
+}
 .btnWrapper{
     display: flex;
-    /* align-items: center; */
     justify-content: space-between;
     margin: 10vh 5% 0 5%;
 }
+.icons{
+    display: none;
+}
+@media only screen and (max-width: 500px){
+button, input[type= submit] {
+    background-color: var(--gray-blue);
+    color: white;
+    border-radius: 8px;
+    font-size: 1.8rem;
+    font-weight:500;
+    cursor: pointer;
+    width: 20vw;
+    height: 42px;
+    border: none;
+}
+.btnWrapper{
+    display: flex;
+    justify-content: space-between;
+    margin: 10vh 5% 0 5%;
+}
+.back-continue{
+    display: none;
+}
+.icons{
+    display: block;
+}
+}
 </style>
-
 <div class="btnWrapper">
     <a href="/login">
-        <button>Back</button>
+        <button class="back-continue">Back</button>
+        <button class="icons"><i class="mi mi-chevron-left"><span class="u-sr-only"></span></i></button>
     </a>
     <a href="/emailVerification">
-        <button>Continue</button>
+        <input type="submit" value="Continue" class="back-continue">
+        <input type="submit" value=">" class="icons">
     </a>
 </div>
--- src/routes/(account)/signup/+page.svelte
+++ src/routes/(account)/signup/+page.svelte
@@ -2,7 +2,7 @@
 
     import '@public/css/style.css';
     import BackConBtn from '@components/common/BackConBtn.svelte';
-    import Hamburger from '../../../components/common/Hamburger.svelte';
+    import AltNav from '../../../components/common/AltNav.svelte';
 
 </script>
 
@@ -19,8 +19,10 @@ input {
 .fullName input{
     width: 100%;
 }
-.educationStateZipWrapper{
+.educationStateZip{
     display: flex;
+    justify-content: space-evenly;
+    gap: 10px;
 }
 .education {
     flex: 2;
@@ -37,53 +39,109 @@ input {
 .passwords{
     display: flex;
     justify-content: space-evenly;
+    gap: 10px;
+}
+.password{
+    width: 100%;
+    flex:1;
+}
+.confirmPassword{
+    width: 100%;
+    flex: 1;
 }
 section{
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    flex: 1;
+    gap: 20px;
+    margin-top: 20px;
+}
+section h1{
+    font-weight: 400;
+    font-size: 2rem;
+}
+section img {
+    width: 340px;
 }
 
+@media only screen and (max-width: 500px) {
+  .formWrap {
+    flex-direction: column;
+    gap: 20px;
+  }
+  form{
+    width:70dvw;
+  }
+  .educationStateZip {
+    flex-direction: column;
+  }
+  .education,
+  .state,
+  .zip {
+    width: 100%;
+  }
+  .passwords {
+    flex-direction: column;
+  }
+  .password,
+  .confirmPassword {
+    width: 100%;
+  }
+  section h1{
+    font-weight: 400;
+    font-size: 5dvw;
+}
+  section img {
+    width: 80%;
+}
+}
 </style>
 
 <div id="page-container" class="height-full">
-    <Hamburger />
+    <AltNav />
     <main>
-        <div class="outterContainer">
-            <div class="createContainer"></div>
-        </div>
-        <div>
+        <section class="container">
+            <h1>Create an Account</h1>
+            <img src="public/images/sign-up-circle-dash.svg" alt="signSvg">
+            <h1>General Information</h1>
             <div class="formWrap">
                 <form action="subimt">
                     <div class="fullName">
                         <label for="name"><p>Full Name</p></label>
                         <input type="text" name="name" id="fullName">
                     </div>
-                    <div class="educationStateZipWrapper">
                         <div class="educationStateZip">
-                            <label for="name"><p>Your Level of Education</p></label>
-                            <input type="text" name="name" id="fullName" class="education">
-                            <label for="name"><p>State</p></label>
-                            <input type="text" name="name" id="fullName" class="state">
-                            <label for="name"><p>Zip Code</p></label>
-                            <input type="text" name="name" id="fullName" class="zip">
+                            <div>
+                                <label for="education"><p>Your Level of Education</p></label>
+                                <input type="text" name="education" id="education" class="education">
+                            </div>
+                            <div>
+                                <label for="state"><p>State</p></label>
+                                <input type="text" name="state" id="state" class="state">
+                            </div>
+                            <div>
+                                <label for="zip"><p>Zip Code</p></label>
+                                <input type="text" name="zip" id="zip" class="zip">
+                            </div>
                         </div>
-                    </div>
                     <div class="email">
-                        <label for="name"><p>Email</p></label>
-                        <input type="text" name="name" id="fullName">
+                        <label for="email"><p>Email</p></label>
+                        <input type="email" name="email" id="email">
                     </div>
                     <div class="passwords">
-                        <label for="name"><p>Password</p></label>
-                        <input type="text" name="name" id="fullName">
-                        <label for="name"><p>Confirmed Password</p></label>
-                        <input type="text" name="name" id="fullName">
+                        <div class="password">
+                            <label for="password"><p>Password</p></label>
+                            <input type="password" name="password" id="password" class="password">
+                        </div>
+                        <div class="confirmPassword">
+                            <label for="confirmPassword"><p>Confirm Password</p></label>
+                            <input type="password" name="confirmPassword" id="confirmPassword" class="confirmPassword">
+                        </div>
                     </div>
                 </form>
             </div>
-        </div>
+        </section>
         <BackConBtn />
     </main>
 </div>
